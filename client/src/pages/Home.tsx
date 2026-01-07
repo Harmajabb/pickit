870ae0dd4bffe37880f968c333365d251f6fbd0b
Juliette De Meo <gigialiasjuliette@gmail.com>
2026-01-07 09:38:00 +0100

    removed component from home

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import About from "../components/About/About";
-import CreateAnnonce from "../components/CreateAnnonce/CreateAnnonce";
 import Hero from "../components/Hero/Hero";
 import ItemHighlight from "../components/ItemHighlight/ItemHighlight";
 import Testimony from "../components/Testimony/Testimony";
@@ -10,7 +9,6 @@ function Home() {
   return (
     <>
       <Hero />
-      <CreateAnnonce />
       <ItemHighlight />
       <About />
       <Testimony />
