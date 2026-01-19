f3aa69a180b2092efa9b202df874e2204579387a
Pierre Fourdin <fourdin.pfmg@gmail.com>
2026-01-19 16:27:24 +0100

    CSS for Dashboard_Admin

--- client/src/pages/Dashboard_Admin/Dashboard_Admin.tsx
+++ client/src/pages/Dashboard_Admin/Dashboard_Admin.tsx
@@ -1,48 +1,84 @@
-import { useContext, useEffect, useState } from "react";
+import { 
+  useContext,
+  useEffect, 
+  useState 
+} from "react";
 import { AuthContext } from "../../context/AuthContext";
 import "./Dashboard_Admin.css";
 import type { Stats } from "./TS-Dashboard_Admin";
+import {
+  User,
+  FlagTriangleRight,
+  ChartBarStacked,
+  MessageCircle,
+} from "lucide-react";
 
 const Dashboard_Admin = () => {
   const { user } = useContext(AuthContext);
-  console.log("État de l'utilisateur :", user);
   const [stats, setStats] = useState<Stats | null>(null);
+
   useEffect(() => {
     if (user?.role === 1) {
       fetch(`${import.meta.env.VITE_API_URL}/api/admin/stats`, {
         credentials: "include",
-      }) // Ajustez l'URL selon votre config
+      })
         .then((res) => res.json())
         .then((data) => setStats(data))
         .catch((err) => console.error("Erreur stats:", err));
     }
   }, [user]);
+
   if (!user || user.role !== 1) {
-    return (
-      <div>Access Denied. You do not have permission to view this page.</div>
-    );
+    return <div>Access Denied.</div>;
   }
+
   return (
     <div className="dashboard-admin">
-      <div className="dashboard-admin-header">
+      <header className="dashboard-admin-header">
         <h1>Admin Dashboard</h1>
-        <p>Welcome, {user?.firstname}!</p>
-      </div>
+        <p>
+          Welcome back, <strong>{user?.firstname}</strong>!
+        </p>
+      </header>
+
       <section className="admin-cockpit">
-        <h2>Admin Cockpit</h2>
-        <div>
-          <p>Number of users currently connected: {stats?.userCount || 0}</p>
-          <p>Number of reports generated: {stats?.reportCount || 0}</p>
-          <p>Number of announcements: {stats?.announcementCount || 0}</p>
+        <div className="stats-items">
+          <div className="stats-item">
+            <h5>Active Users</h5>
+            <span className="stat-value">{stats?.userCount ?? 0}</span>
+          </div>
+
+          <div className="stats-item">
+            <h5>Reports Generated</h5>
+            <span className="stat-value">{stats?.reportCount ?? 0}</span>
+          </div>
+
+          <div className="stats-item">
+            <h5>Announcements</h5>
+            <span className="stat-value">{stats?.announcementCount ?? 0}</span>
+          </div>
         </div>
       </section>
+
       <section>
-        <h2>Admin Controls</h2>
-        <div>
-          <button type="button">Manage Users</button>
-          <button type="button">Review Reports</button>
-          <button type="button">Moderate Announcements</button>
-          <button type="button">Moderate Review</button>
+        <h2 className="title-controls">Admin Controls</h2>
+        <div className="list-btn">
+          <button className="secondary btnusers" type="button">
+            <User size={32} strokeWidth={2} />
+            Manage Users
+          </button>
+          <button className="secondary btnusers" type="button">
+            <FlagTriangleRight size={32} strokeWidth={2} />
+            Manage Reports
+          </button>
+          <button className="secondary btnusers" type="button">
+            <ChartBarStacked size={32} strokeWidth={2} />
+            Manage Category
+          </button>
+          <button className="secondary btnusers" type="button">
+            <MessageCircle size={32} strokeWidth={2} />
+            Chats
+          </button>
         </div>
       </section>
     </div>
