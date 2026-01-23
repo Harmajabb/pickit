a9971c24d97563b30de2e30ddb3d23d91665a94c
LEA FRANCOIS <lea.jeane.francois@gmail.com>
2026-01-23 10:43:22 +0100

    refactor - update navbar link

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -82,7 +82,7 @@ function Navbar() {
         {/* CHANGE CONDITION : IF USER IS LOGGED IN -> SHOW PROFILE ICON AND DISCONNECT BTN (FIST DIV) IF HES NOT LOGGED IN -> SHOW LOG IN / SIGN IN BTN */}
         {isLogged ? (
           <>
-            <Link to="/profile/me">
+            <Link to={user?.role === 1 ? "/ad-dashboard" : "/profile/me"}>
               <svg aria-hidden="true" viewBox="0 0 14 20" className="nav-icons">
                 <path
                   d="M3 5C3 6.06087 3.42143 7.07828 4.17157 7.82843C4.92172 8.57857 5.93913 9 7 9C8.06087 9 9.07828 8.57857 9.82843 7.82843C10.5786 7.07828 11 6.06087 11 5C11 3.93913 10.5786 2.92172 9.82843 2.17157C9.07828 1.42143 8.06087 1 7 1C5.93913 1 4.92172 1.42143 4.17157 2.17157C3.42143 2.92172 3 3.93913 3 5Z"
@@ -113,7 +113,7 @@ function Navbar() {
         <img src={roundedLogo} alt="PICKIT logo" className="mobile-nav_logo" />
       </Link>
       <nav className="mobile-nav">
-        <Link to="">
+        <Link to="/catalog">
           <svg viewBox="0 0 20 20" aria-hidden="true" className="nav-icons">
             <path
               d="M1 8C1 8.91925 1.18106 9.82951 1.53284 10.6788C1.88463 11.5281 2.40024 12.2997 3.05025 12.9497C3.70026 13.5998 4.47194 14.1154 5.32122 14.4672C6.1705 14.8189 7.08075 15 8 15C8.91925 15 9.82951 14.8189 10.6788 14.4672C11.5281 14.1154 12.2997 13.5998 12.9497 12.9497C13.5998 12.2997 14.1154 11.5281 14.4672 10.6788C14.8189 9.82951 15 8.91925 15 8C15 7.08075 14.8189 6.1705 14.4672 5.32122C14.1154 4.47194 13.5998 3.70026 12.9497 3.05025C12.2997 2.40024 11.5281 1.88463 10.6788 1.53284C9.82951 1.18106 8.91925 1 8 1C7.08075 1 6.1705 1.18106 5.32122 1.53284C4.47194 1.88463 3.70026 2.40024 3.05025 3.05025C2.40024 3.70026 1.88463 4.47194 1.53284 5.32122C1.18106 6.1705 1 7.08075 1 8Z"
@@ -130,7 +130,7 @@ function Navbar() {
           </svg>
           <span>Search</span>
         </Link>
-        <Link to="">
+        <Link to="/">
           <svg viewBox="0 0 20 20" aria-hidden="true" className="nav-icons">
             <path
               d="M3 10H1L10 1L19 10H17"
@@ -179,7 +179,7 @@ function Navbar() {
           </svg>
           <span>Chat</span>
         </Link>
-        <Link to="">
+        <Link to={user?.role === 1 ? "/ad-dashboard" : "/profile/me"}>
           <svg viewBox="0 0 14 20" aria-hidden="true" className="nav-icons">
             <path
               d="M3 5C3 6.06087 3.42143 7.07828 4.17157 7.82843C4.92172 8.57857 5.93913 9 7 9C8.06087 9 9.07828 8.57857 9.82843 7.82843C10.5786 7.07828 11 6.06087 11 5C11 3.93913 10.5786 2.92172 9.82843 2.17157C9.07828 1.42143 8.06087 1 7 1C5.93913 1 4.92172 1.42143 4.17157 2.17157C3.42143 2.92172 3 3.93913 3 5Z"
