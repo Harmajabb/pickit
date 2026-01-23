0173d9d66353b747c17e6d6085201e567d17e0c3
LEA FRANCOIS <lea.jeane.francois@gmail.com>
2026-01-23 10:57:54 +0100

    refactor - adding condition for user 0 and admin 1

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -43,18 +43,18 @@ function Footer() {
             aria-label="Meta"
             className="social-icon"
           >
-            <g clip-path="url(#clip0_105_1574)">
+            <g clipPath="url(#clip0_105_1574)">
               <path
                 d="M12 10.174C13.766 7.39 15.315 6 16.648 6C18.648 6 19.911 8.213 20.648 11.217C21.352 14.086 21.148 18 18.648 18C17.534 18 16 16.435 14.5 14.348C13.5455 13.0329 12.709 11.6361 12 10.174Z"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
               <path
                 d="M12.0001 10.174C10.2341 7.39 8.68512 6 7.35212 6C5.35212 6 4.08911 8.213 3.35211 11.217C2.64811 14.086 2.85211 18 5.35211 18C6.46611 18 8.00011 16.435 9.50011 14.348C10.5001 12.957 11.3331 11.565 12.0001 10.174Z"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
             </g>
             <defs>
